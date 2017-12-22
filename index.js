814c5381d50129a63d0f98b140cdb348198d3e6e
Leonard Nuernberg <lenny@moontec.de>
2017-12-22 11:43:28 +0100

    basic package setup

---
+++ index.js
@@ -0,0 +1,13 @@
+//import fs from 'fs';
+//import Animal from 'path/to/Animal.js';
+
+export default class SweetprojectsAPI {
+
+  constructor(name){
+    this.name = name ;
+  }
+
+  print(){
+    console.log('Name is :'+ this.name);
+  }
+}
